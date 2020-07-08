0bca3851dc516f57252795bc67889c57304a7e66
prash897 <prashantchouhan9@gmail.com>
2020-07-08 17:05:33 +0530

    mitigate compile error : temp

--- src/main/java/com/capitalone/dashboard/webhook/github/GitHubSyncServiceImpl.java
+++ src/main/java/com/capitalone/dashboard/webhook/github/GitHubSyncServiceImpl.java
@@ -1,11 +1,38 @@
 package com.capitalone.dashboard.webhook.github;
 
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.joda.time.DateTime;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+
 import com.capitalone.dashboard.client.RestClient;
 import com.capitalone.dashboard.client.RestUserInfo;
 import com.capitalone.dashboard.misc.HygieiaException;
-import com.capitalone.dashboard.model.CollectionError;
 import com.capitalone.dashboard.model.CollectionMode;
-import com.capitalone.dashboard.model.Collector;
 import com.capitalone.dashboard.model.Comment;
 import com.capitalone.dashboard.model.Commit;
 import com.capitalone.dashboard.model.CommitStatus;
@@ -28,36 +55,6 @@ import com.capitalone.dashboard.util.Encryption;
 import com.capitalone.dashboard.util.EncryptionException;
 import com.capitalone.dashboard.util.GithubGraphQLQuery;
 import com.google.common.collect.Lists;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.MapUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.math.NumberUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.joda.time.DateTime;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestClientException;
-
-import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 @Service
 public class GitHubSyncServiceImpl implements GitHubSyncService {
@@ -102,93 +99,84 @@ public class GitHubSyncServiceImpl implements GitHubSyncService {
     }
 
     @Override
-    public String syncGithubRepo(GitSyncRequest request) throws ParseException, HygieiaException, MalformedURLException {
-
-        int commitCount = 0;
-        int pullCount = 0;
-        int issueCount = 0;
-        String statusString;
-        long start = System.currentTimeMillis();
-        int repoCount = 0;
-        apiSettings.getGithubSyncSettings().setFirstRunHistoryDays(request.getHistoryDays());
-        try {
-            Collector collector = collectorRepository.findByName(GIT_HUB);
-            if (Objects.isNull(collector)) return "Invalid collector";
-            List<GitHubRepo> repos = gitHubRepoRepository.findRepoByUrlAndBranch(collector.getId(), request.getRepo(), request.getBranch());
-            for (GitHubRepo repo : repos) {
-                try {
-                    collectorItemRepository.save(repo);
-                    List<GitRequest> allRequests = gitRequestRepository.findRequestNumberAndLastUpdated(repo.getId());
-
-                    Map<Long, String> existingPRMap = allRequests.stream().filter(r -> Objects.equals(r.getRequestType(), "pull")).collect(
-                            Collectors.toMap(GitRequest::getUpdatedAt, GitRequest::getNumber,
-                                    (oldValue, newValue) -> oldValue
-                            )
-                    );
-
-                    Map<Long, String> existingIssueMap = allRequests.stream().filter(r -> Objects.equals(r.getRequestType(), "issue")).collect(
-                            Collectors.toMap(GitRequest::getUpdatedAt, GitRequest::getNumber,
-                                    (oldValue, newValue) -> oldValue
-                            )
-                    );
-
-                    fireGraphQL(repo, true, existingPRMap, existingIssueMap);
-                    int commitCount1 = processCommits(repo);
-                    commitCount += commitCount1;
-
-                    //Get all the Pull Requests
-                    int pullCount1 = processPRorIssueList(repo, allRequests.stream().filter(r -> Objects.equals(r.getRequestType(), "pull")).collect(Collectors.toList()), "pull");
-                    pullCount += pullCount1;
-
-                    //Get all the Issues
-                    int issueCount1 = processPRorIssueList(repo, allRequests.stream().filter(r -> Objects.equals(r.getRequestType(), "issue")).collect(Collectors.toList()), "issue");
-                    issueCount += issueCount;
-
-                    // Due to timing of PRs and Commits in PR merge event, some commits may not be included in the response and will not be connected to a PR.
-                    // This is the place attempting to re-connect the commits and PRs in case they were missed during previous run.
-
-                    processOrphanCommits(repo);
-
-                    repo.setLastUpdated(System.currentTimeMillis());
-                    // if everything went alright, there should be no error!
-                    repo.getErrors().clear();
-                    statusString = "SUCCESS, pulls=" + pullCount1 + ", commits=" + pullCount1 + ", issues=" + issueCount1;
-                    repoCount++;
-
-                } catch (HttpStatusCodeException hc) {
-                    LOG.error("Error fetching commits for:" + repo.getRepoUrl(), hc);
-                    statusString = "EXCEPTION, " + hc.getClass().getCanonicalName();
-                    CollectionError error = new CollectionError(hc.getStatusCode().toString(), hc.getMessage());
-                    if (hc.getStatusCode() == HttpStatus.UNAUTHORIZED || hc.getStatusCode() == HttpStatus.FORBIDDEN) {
-                        LOG.info("add 0.2 sec delay when received 401/403 from GitHub");
-
-                    }
-                    repo.getErrors().add(error);
-                } catch (RestClientException | MalformedURLException ex) {
-                    LOG.error("Error fetching commits for:" + repo.getRepoUrl(), ex);
-                    statusString = "EXCEPTION, " + ex.getClass().getCanonicalName();
-                    CollectionError error = new CollectionError(CollectionError.UNKNOWN_HOST, ex.getMessage());
-                    repo.getErrors().add(error);
-                } catch (HygieiaException he) {
-                    LOG.error("Error fetching commits for:" + repo.getRepoUrl(), he);
-                    statusString = "EXCEPTION, " + he.getClass().getCanonicalName();
-                    CollectionError error = new CollectionError(String.valueOf(he.getErrorCode()), he.getMessage());
-                    repo.getErrors().add(error);
-                }
-                gitHubRepoRepository.save(repo);
-            }
-        } catch (Throwable e) {
-            statusString = "EXCEPTION, " + e.getClass().getCanonicalName();
-            LOG.error("Unexpected exception when collecting : " + statusString, e);
-        }
-        long end = System.currentTimeMillis();
-        long elapsedSeconds = (end - start) / 1000;
-        String format = String.format("GithubSyncServiceImpl : totalProcessSeconds=%d, totalRepoCount=%d, totalNewPulls=%d, totalNewCommits=%d totalNewIssues=%d",
-                elapsedSeconds, repoCount, pullCount, commitCount, issueCount);
-        LOG.info(format);
-        return format;
-
-    }
+	public String syncGithubRepo(GitSyncRequest request)
+			throws ParseException, HygieiaException, MalformedURLException {
+    	return null;
+		/*
+		 * 
+		 * int commitCount = 0; int pullCount = 0; int issueCount = 0; String
+		 * statusString; long start = System.currentTimeMillis(); int repoCount = 0;
+		 * apiSettings.getGithubSyncSettings().setFirstRunHistoryDays(request.
+		 * getHistoryDays()); try { Collector collector =
+		 * collectorRepository.findByName(GIT_HUB); if (Objects.isNull(collector))
+		 * return "Invalid collector"; List<GitHubRepo> repos =
+		 * gitHubRepoRepository.findRepoByUrlAndBranch(collector.getId(),
+		 * request.getRepo(), request.getBranch()); for (GitHubRepo repo : repos) { try
+		 * { collectorItemRepository.save(repo); List<GitRequest> allRequests =
+		 * gitRequestRepository.findRequestNumberAndLastUpdated(repo.getId());
+		 * 
+		 * Map<Long, String> existingPRMap = allRequests.stream().filter(r ->
+		 * Objects.equals(r.getRequestType(), "pull")).collect(
+		 * Collectors.toMap(GitRequest::getUpdatedAt, GitRequest::getNumber, (oldValue,
+		 * newValue) -> oldValue ) );
+		 * 
+		 * Map<Long, String> existingIssueMap = allRequests.stream().filter(r ->
+		 * Objects.equals(r.getRequestType(), "issue")).collect(
+		 * Collectors.toMap(GitRequest::getUpdatedAt, GitRequest::getNumber, (oldValue,
+		 * newValue) -> oldValue ) );
+		 * 
+		 * fireGraphQL(repo, true, existingPRMap, existingIssueMap); int commitCount1 =
+		 * processCommits(repo); commitCount += commitCount1;
+		 * 
+		 * //Get all the Pull Requests int pullCount1 = processPRorIssueList(repo,
+		 * allRequests.stream().filter(r -> Objects.equals(r.getRequestType(),
+		 * "pull")).collect(Collectors.toList()), "pull"); pullCount += pullCount1;
+		 * 
+		 * //Get all the Issues int issueCount1 = processPRorIssueList(repo,
+		 * allRequests.stream().filter(r -> Objects.equals(r.getRequestType(),
+		 * "issue")).collect(Collectors.toList()), "issue"); issueCount += issueCount;
+		 * 
+		 * // Due to timing of PRs and Commits in PR merge event, some commits may not
+		 * be included in the response and will not be connected to a PR. // This is the
+		 * place attempting to re-connect the commits and PRs in case they were missed
+		 * during previous run.
+		 * 
+		 * processOrphanCommits(repo);
+		 * 
+		 * repo.setLastUpdated(System.currentTimeMillis()); // if everything went
+		 * alright, there should be no error! repo.getErrors().clear(); statusString =
+		 * "SUCCESS, pulls=" + pullCount1 + ", commits=" + pullCount1 + ", issues=" +
+		 * issueCount1; repoCount++;
+		 * 
+		 * } catch (HttpStatusCodeException hc) {
+		 * LOG.error("Error fetching commits for:" + repo.getRepoUrl(), hc);
+		 * statusString = "EXCEPTION, " + hc.getClass().getCanonicalName();
+		 * CollectionError error = new CollectionError(hc.getStatusCode().toString(),
+		 * hc.getMessage()); if (hc.getStatusCode() == HttpStatus.UNAUTHORIZED ||
+		 * hc.getStatusCode() == HttpStatus.FORBIDDEN) {
+		 * LOG.info("add 0.2 sec delay when received 401/403 from GitHub");
+		 * 
+		 * } repo.getErrors().add(error); } catch (RestClientException |
+		 * MalformedURLException ex) { LOG.error("Error fetching commits for:" +
+		 * repo.getRepoUrl(), ex); statusString = "EXCEPTION, " +
+		 * ex.getClass().getCanonicalName(); CollectionError error = new
+		 * CollectionError(CollectionError.UNKNOWN_HOST, ex.getMessage());
+		 * repo.getErrors().add(error); } catch (HygieiaException he) {
+		 * LOG.error("Error fetching commits for:" + repo.getRepoUrl(), he);
+		 * statusString = "EXCEPTION, " + he.getClass().getCanonicalName();
+		 * CollectionError error = new
+		 * CollectionError(String.valueOf(he.getErrorCode()), he.getMessage());
+		 * repo.getErrors().add(error); } gitHubRepoRepository.save(repo); } } catch
+		 * (Throwable e) { statusString = "EXCEPTION, " +
+		 * e.getClass().getCanonicalName();
+		 * LOG.error("Unexpected exception when collecting : " + statusString, e); }
+		 * long end = System.currentTimeMillis(); long elapsedSeconds = (end - start) /
+		 * 1000; String format = String.
+		 * format("GithubSyncServiceImpl : totalProcessSeconds=%d, totalRepoCount=%d, totalNewPulls=%d, totalNewCommits=%d totalNewIssues=%d"
+		 * , elapsedSeconds, repoCount, pullCount, commitCount, issueCount);
+		 * LOG.info(format); return format;
+		 * 
+		 */}
 
     public List<Commit> getCommits() {
         return commits;
